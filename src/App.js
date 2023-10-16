52d484a405f3226a4a52c6a1c8863517b487bdbb
suraj <contactsuraj6299@gmail.com>
2023-10-16 23:20:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Container from './components/container';
+
+function App() {
+  return (
+    <div ClassName='bg-dark container-fluid'>
+        <Container/>
+    </div>
+  );
+}
+
+export default App;
